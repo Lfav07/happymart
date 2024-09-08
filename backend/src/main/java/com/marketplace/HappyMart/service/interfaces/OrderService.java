44862f24350e8c6e10643ab43eef792e779414e4
lfav07 <lfavieira07@gmail.com>
2024-09-08 18:38:29 -0300

    Implemented OrderService interface

---
+++ backend/src/main/java/com/marketplace/HappyMart/service/interfaces/OrderService.java
@@ -0,0 +1,16 @@
+package com.marketplace.HappyMart.service.interfaces;
+
+import com.marketplace.HappyMart.model.Order;
+import com.marketplace.HappyMart.model.OrderStatus;
+
+import java.util.List;
+
+public interface OrderService {
+
+    public Order createOrder(Order order);
+    public List<Order> getAllOrders();
+    public Order getOrderById(Long id);
+    public Order updateOrder(Long id, Long userId, int totalAmount, OrderStatus status);
+    public void deleteOrderById(Long id);
+    public void deleteAllProducts();
+}
