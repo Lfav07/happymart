46a129a6e4abf505e913b981c5efaf72606d43aa
lfav07 <lfavieira07@gmail.com>
2024-09-09 10:43:46 -0300

    Implemented OrderItemService

---
+++ backend/src/main/java/com/marketplace/HappyMart/service/OrderItemServiceImpl.java
@@ -0,0 +1,38 @@
+package com.marketplace.HappyMart.service;
+
+import com.marketplace.HappyMart.model.Order;
+import com.marketplace.HappyMart.model.OrderItem;
+import com.marketplace.HappyMart.repository.OrderItemRepository;
+import com.marketplace.HappyMart.repository.OrderRepository;
+import com.marketplace.HappyMart.service.interfaces.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderItemServiceImpl implements OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderItemServiceImpl(OrderItemRepository orderItemRepository, OrderRepository orderRepository) {
+        this.orderItemRepository = orderItemRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public OrderItem createOrderItem(OrderItem orderItem) {
+        Order order = orderRepository.findById(orderItem.getOrder().getId())
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        orderItem.setOrder(order);
+        return orderItemRepository.save(orderItem);
+    }
+
+    @Override
+    public List<OrderItem> getOrderItemsByOrderId(Long orderId) {
+        return orderItemRepository.findByOrderId(orderId);
+    }
+}
