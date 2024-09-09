f83d3aef60193317271488695ab8d922afa3b622
lfav07 <lfavieira07@gmail.com>
2024-09-09 10:43:38 -0300

    Implemented OrderItemService interface

---
+++ backend/src/main/java/com/marketplace/HappyMart/service/interfaces/OrderItemService.java
@@ -0,0 +1,11 @@
+package com.marketplace.HappyMart.service.interfaces;
+
+import com.marketplace.HappyMart.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderItemService {
+
+    public OrderItem createOrderItem(OrderItem orderItem);
+    public List<OrderItem> getOrderItemsByOrderId(Long orderId);
+}
