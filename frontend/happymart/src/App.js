540ce81dfb4c21a55de4c069aeb3eb9395cfe27e
lfav07 <lfavieira07@gmail.com>
2024-09-16 17:53:39 -0300

    Routes and userContext configuration

--- frontend/happymart/src/App.js
+++ frontend/happymart/src/App.js
@@ -2,22 +2,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
+import { UserProvider } from './contexts/UserContext'; // Adjust path as needed
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import CompleteProductList from './pages/CompleteProductList';
 import ProtectedRoute from './components/ProtectedRoute';
+import HomePage from './pages/HomePage'
+import CartPage from './pages/CartPage'
+import LogoutPage from './pages/LogoutPage'
 
 const App = () => {
     return (
+    <UserProvider>
         <Router>
             <Routes>
                 <Route path="/" element={<IndexPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/products" element={<ProtectedRoute element={CompleteProductList} />} />
-
+                <Route path="/cart" element={<ProtectedRoute element={CartPage} />} />
+                <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
+                <Route path="/logout" element={<ProtectedRoute element={LogoutPage} />} />
             </Routes>
         </Router>
+        </UserProvider>
     );
 };
 
