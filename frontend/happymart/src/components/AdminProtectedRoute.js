2c260643ac40b708ab18c25dc27df0b47dd77c6f
lfav07 <lfavieira07@gmail.com>
2024-09-18 15:47:33 -0300

    Updated AdminProtectedRoute.js

--- frontend/happymart/src/components/AdminProtectedRoute.js
+++ frontend/happymart/src/components/AdminProtectedRoute.js
@@ -1,12 +1,10 @@
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const AdminProtectedRoute = ({ element: Component, ...rest }) => {
+const AdminProtectedRoute = ({ element: Element, ...rest }) => {
     const token = localStorage.getItem('jwt');
 
-
-    return token ? <Component {...rest} /> : <Navigate to="/admin/login" replace />;
+    return token ? <Element {...rest} /> : <Navigate to="/admin/login" replace />;
 };
 
 export default AdminProtectedRoute;
