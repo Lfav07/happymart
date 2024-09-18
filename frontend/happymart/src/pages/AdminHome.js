7ba11b2561ffd519d874c574ba6ca724741b12d2
lfav07 <lfavieira07@gmail.com>
2024-09-18 15:46:43 -0300

    Updated AdminHome.js

--- frontend/happymart/src/pages/AdminHome.js
+++ frontend/happymart/src/pages/AdminHome.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const HomePage = () => {
+const AdminHome = () => {
     const navigate = useNavigate();
     const currentUsername = localStorage.getItem('username');
 
     return (
         <div>
             <h1>HappyMarket</h1>
-            <h2>Welcome, {currentUsername ? currentUsername : 'Guest'}!</h2>
+            <h2>Welcome, Administrator {currentUsername ? currentUsername : 'Guest'}!</h2>
             <p>Choose an action:</p>
             <button onClick={() => navigate('/products')}>Go to products catalog</button>
-            <button onClick={() => navigate('/admin/products')}>Manage products</button>
-            <button onClick={() => navigate('/userslist')}>Manage users </button>
-            <button onClick={() => navigate('/logout')}>Log out</button>
+            <button onClick={() => navigate('/admin/AdminProductsPage')}>Manage products</button>
+            <button onClick={() => navigate('/admin/userslist')}>Manage users </button>
+            <button onClick={() => navigate('/admin/logout')}>Log out</button>
         </div>
     );
 };
 
-export default HomePage;
+export default AdminHome;
