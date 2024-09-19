bac60691392f73b6211171c4984760cd179cea0e
lfav07 <lfavieira07@gmail.com>
2024-09-19 14:58:01 -0300

    Updated CategoryRepository

--- backend/src/main/java/com/marketplace/HappyMart/repository/CategoryRepository.java
+++ backend/src/main/java/com/marketplace/HappyMart/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.marketplace.HappyMart.repository;
 import com.marketplace.HappyMart.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
