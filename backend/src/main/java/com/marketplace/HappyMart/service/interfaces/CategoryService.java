eb9b1dd71a3a15ab326e07ee825d0cc22605e3d4
lfav07 <lfavieira07@gmail.com>
2024-09-19 14:58:08 -0300

    Updated CategoryService

--- backend/src/main/java/com/marketplace/HappyMart/service/interfaces/CategoryService.java
+++ backend/src/main/java/com/marketplace/HappyMart/service/interfaces/CategoryService.java
@@ -1,12 +1,17 @@
 package com.marketplace.HappyMart.service.interfaces;
 
 import com.marketplace.HappyMart.model.Category;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
     public Category createCategory(Category category);
+
+    @Transactional
+    Optional<Category> createCategoryByName(String name);
+
     public List<Category> getAllCategories();
     public Optional<Category> getCategoryById(Long id);
     public Optional<Category> updateCategory(Long id, String name);
