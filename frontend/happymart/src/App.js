8281c36825f823c7109e5a8dff765d0e3decd802
lfav07 <lfavieira07@gmail.com>
2024-09-19 14:57:52 -0300

    Updated App.js

--- frontend/happymart/src/App.js
+++ frontend/happymart/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
-import { UserProvider } from './contexts/UserContext'; // Adjust path as needed
+import { UserProvider } from './contexts/UserContext';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import CompleteProductList from './pages/CompleteProductList';
@@ -12,10 +12,9 @@ import CartPage from './pages/CartPage';
 import LogoutPage from './pages/LogoutPage';
 import AdminLoginPage from './pages/AdminLoginPage';
 import AdminLogoutPage from './pages/AdminLogoutPage';
-//import AdminProductsPage from './pages/AdminProductsPage';
-//import AddProductPage from './pages/AddProductPage';
-//import EditProductPage from './pages/EditProductPage';
- import AdminHome from './pages/AdminHome';
+import AdminProductsPage from './pages/AdminProductsPage';
+import AddProductPage from './pages/AddProductPage';
+import AdminHome from './pages/AdminHome';
 
 const App = () => {
     return (
@@ -25,15 +24,15 @@ const App = () => {
                     <Route path="/" element={<IndexPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/products" element={<ProtectedRoute><CompleteProductList /></ProtectedRoute>} />
-                    <Route path="/cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
-                    <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-                    <Route path="/logout" element={<ProtectedRoute><LogoutPage /></ProtectedRoute>} />
+                    <Route path="/products" element={<ProtectedRoute element={CompleteProductList} />} />
+                    <Route path="/cart" element={<ProtectedRoute element={CartPage} />} />
+                    <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
+                    <Route path="/logout" element={<ProtectedRoute element={LogoutPage} />} />
                     <Route path="/admin/login" element={<AdminLoginPage />} />
-                   <Route path="/admin/home" element={<AdminProtectedRoute element={AdminHome} />} />
-                   <Route path="/admin/logout" element={<AdminProtectedRoute element={AdminLogoutPage} />} />
-
-
+                    <Route path="/admin/products" element={<AdminProtectedRoute element={AdminProductsPage} />} />
+                    <Route path="/admin/products/add" element={<AdminProtectedRoute element={AddProductPage} />} />
+                    <Route path="/admin/home" element={<AdminProtectedRoute element={AdminHome} />} />
+                    <Route path="/admin/logout" element={<AdminProtectedRoute element={AdminLogoutPage} />} />
                 </Routes>
             </Router>
         </UserProvider>
