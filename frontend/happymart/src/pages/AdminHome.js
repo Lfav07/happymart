87785a97be25b7dd0776a01f67379185ba60e83f
lfav07 <lfavieira07@gmail.com>
2024-09-22 14:57:53 -0300

    Fixed routing

--- frontend/happymart/src/pages/AdminHome.js
+++ frontend/happymart/src/pages/AdminHome.js
@@ -10,9 +10,9 @@ const AdminHome = () => {
             <h1>HappyMarket</h1>
             <h2>Welcome, Administrator {currentUsername ? currentUsername : 'Guest'}!</h2>
             <p>Choose an action:</p>
-            <button onClick={() => navigate('/products')}>Go to products catalog</button>
-            <button onClick={() => navigate('/admin/AdminProductsPage')}>Manage products</button>
-            <button onClick={() => navigate('/admin/userslist')}>Manage users </button>
+            <button onClick={() => navigate('/admin/products')}>Manage products</button>
+            <button onClick={() => navigate('/admin/carts')}>Manage Carts</button>
+            <button onClick={() => navigate('/admin/users')}>Manage users </button>
             <button onClick={() => navigate('/admin/logout')}>Log out</button>
         </div>
     );
