bb33b8e922900ade5a09164dcec8100f5926bc40
lfav07 <lfavieira07@gmail.com>
2024-10-01 18:20:22 -0300

    Added utilites for CSS

--- frontend/happymart/src/pages/AdminHome.js
+++ frontend/happymart/src/pages/AdminHome.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './css/AdminHome.css';
 
 const AdminHome = () => {
     const navigate = useNavigate();
     const currentUsername = localStorage.getItem('username');
 
     return (
-        <div>
+        <div className="admin-home-container">
             <h1>HappyMarket</h1>
-            <h2>Welcome, Administrator {currentUsername ? currentUsername : 'Guest'}!</h2>
+            <h2>Welcome, Administrator!</h2>
             <p>Choose an action:</p>
             <button onClick={() => navigate('/admin/products')}>Manage products</button>
             <button onClick={() => navigate('/admin/carts')}>Manage Carts</button>
-            <button onClick={() => navigate('/admin/users')}>Manage users </button>
+            <button onClick={() => navigate('/admin/users')}>Manage users</button>
             <button onClick={() => navigate('/admin/logout')}>Log out</button>
         </div>
+
     );
 };
 
