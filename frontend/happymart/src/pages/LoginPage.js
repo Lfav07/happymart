a4f831759f50b31f3c513c160aa733c91f57398e
lfav07 <lfavieira07@gmail.com>
2024-10-02 18:37:01 -0300

    Updated HTML tags

--- frontend/happymart/src/pages/LoginPage.js
+++ frontend/happymart/src/pages/LoginPage.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './css/UserLoginPage.css';
 
 const LoginPage = ({ onLogin }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -15,43 +16,58 @@ const LoginPage = ({ onLogin }) => {
                 username,
                 password
             });
-             const { token, user } = response.data;
+            const { token, user } = response.data;
             localStorage.setItem('jwt', token);
             localStorage.setItem('userId', user.id);
             localStorage.setItem('username', user.username);
-            setMessage('Login successful!, Redirecting to home page!');
-             setTimeout(() => {
-                            navigate('/home');
-                        }, 2000);
+            setMessage('Login successful! Redirecting to home page!');
+            setTimeout(() => {
+                navigate('/home');
+            }, 2000);
             if (onLogin) onLogin();
         } catch (error) {
             setMessage(error.response ? error.response.data : 'Error occurred, Please try again');
         }
     };
 
-
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="container">
+
+                <div className="happytext">HappyMarket</div>
+
+            <div className="text">Login</div>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+                <div className="form-row">
+                    <div className="input-data">
+                        <input
+                            type="text"
+                            required
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <label>Username</label>
+                        <div className="underline"></div>
+                    </div>
                 </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                <div className="form-row">
+                    <div className="input-data">
+                        <input
+                            type="password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label>Password</label>
+                        <div className="underline"></div>
+                    </div>
                 </div>
-                <button type="submit">Login</button>
-                <button onClick={() => navigate('/register')}>Go to Register</button>
+                <div className="form-row submit-btn">
+                    <div className="input-data">
+                        <div className="inner"></div>
+                        <input type="submit" value="Login" />
+                    </div>
+                </div>
+                <button type="button" onClick={() => navigate('/register')}>Go to Register</button>
             </form>
             {message && <p>{message}</p>}
         </div>
