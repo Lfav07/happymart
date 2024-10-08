adab9c03f913c623ccc88e57d54a86febd95520d
lfav07 <lfavieira07@gmail.com>
2024-10-08 11:12:18 -0300

    Initial pt-br translation i18n

---
+++ frontend/happymart/src/i18n.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(HttpApi)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- frontend/happymart/src/pages/HomePage.js
+++ frontend/happymart/src/pages/HomePage.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+    const { t, i18n } = useTranslation();
     const navigate = useNavigate();
-    const currentUsername = localStorage.getItem('username');
+    const currentUsername = localStorage.getItem('username') || t('guest');
 
     return (
         <div>
-            <h1>HappyMarket</h1>
-            <h2>Welcome, {currentUsername ? currentUsername : 'Guest'}!</h2>
-            <p>Choose an action:</p>
-            <button onClick={() => navigate('/products')}>Products catalog</button>
-            <button onClick={() => navigate('/cart')}>Cart</button>
-            <button onClick={() => navigate('/orders')}>Orders</button>
-            <button onClick={() => navigate('/logout')}>Log out</button>
+            <h1>{t('app_name')}</h1> {}
+
+            <button onClick={() => i18n.changeLanguage('pt-BR')}>Português (Brasil)</button>
+            <button onClick={() => i18n.changeLanguage('en')}>English</button>
+
+            <h2>{t('welcome', { name: currentUsername })}</h2> {}
+            <p>{t('choose_action')}</p> {}
+            <button onClick={() => navigate('/products')}>{t('products_catalog')}</button> {}
+            <button onClick={() => navigate('/cart')}>{t('cart')}</button>
+            <button onClick={() => navigate('/orders')}>{t('orders')}</button>
+            <button onClick={() => navigate('/logout')}>{t('logout')}</button>
         </div>
     );
 };
